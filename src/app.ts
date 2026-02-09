efca9044a5d76ed9b3080c65a90ef07f3b162380
Emanuel Reino <emanuelreino13@gmail.com>
2026-02-09 10:30:38 -0300

    fix: vercel allowed by cors policy

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,25 @@ const app = express();
 app.use(passport.initialize());
 configurePassport(passport);
 
-const corsOptions = {
-  origin: ["http://localhost:5173"],
-  credentials: true,
-};
+const allowedOrigins = [
+  "https://maestro-phi.vercel.app",
+  "http://localhost:5173",
+];
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS policy"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use("/user", UserRoutes);
 app.use("/songs", SongRoutes);
